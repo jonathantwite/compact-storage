584c5537f7e1b49aa5d381743f49549b54607dd5
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2021-03-16 20:27:21 +0000

    init(Test): Setup Jest

---
+++ tests/storage.test.ts
@@ -0,0 +1,16 @@
+import { save } from '../src/storage';
+
+class TestObj{
+    prop1: number;
+    prop2: string;
+};
+
+describe('LocalStorage called', () => {
+    it('Should be called', () =>{
+        let obj = new TestObj();
+            obj.prop1= 15;
+            obj.prop2= 'Hello';
+
+        expect(save<TestObj>("test", obj)).toBe(2);
+    })
+});
